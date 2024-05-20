3ecc8e471bedcd858aa2b92025a9e4852924eb69
makim0939 <95673492+makim0939@users.noreply.github.com>
2024-05-21 00:00:07 +0900

    upgrade main.ts to display joint locations

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import "./style.css";
 import { FilesetResolver, HandLandmarker } from "@mediapipe/tasks-vision";
+import {
+  NormalizedLandmarkListList,
+  drawConnectors,
+  drawLandmarks,
+} from "@mediapipe/drawing_utils";
+import { HAND_CONNECTIONS } from "@mediapipe/hands";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <video id="video" autoplay></video>
+    <canvas id="canvas"></canvas>
   </div>
 `;
 const video = document.getElementById("video") as HTMLVideoElement;
 navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
   video.srcObject = stream;
 });
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d")!;
 
 const vision = await FilesetResolver.forVisionTasks(
   // path/to/wasm/root
@@ -27,12 +36,37 @@ const handLandmarker = await HandLandmarker.createFromOptions(vision, {
 let lastVideoTime = -1;
 function renderLoop() {
   if (video.currentTime !== lastVideoTime) {
-    const detections = handLandmarker.detectForVideo(video, video.currentTime);
-    detections.landmarks.forEach((landmarks) => {
-      console.log(landmarks);
-    });
+    const results = handLandmarker.detectForVideo(video, video.currentTime);
     lastVideoTime = video.currentTime;
+    ctx.save();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.scale(-1, 1);
+    ctx.translate(-canvas.width, 0);
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    if (results.landmarks.length > 0) {
+      for (const landmarks of results.landmarks) {
+        console.log(landmarks);
+
+        for (const landmark of landmarks) {
+          ctx.fillStyle = "#0000ff";
+          ctx.fillRect(
+            landmark.x * canvas.width,
+            landmark.y * canvas.height,
+            10,
+            10
+          );
+        }
+      }
+    }
   }
+
+  ctx.restore();
   window.requestAnimationFrame(renderLoop);
 }
-video.addEventListener("loadeddata", renderLoop);
+video.addEventListener("loadeddata", () => {
+  canvas.style.width = "" + video.videoWidth;
+  canvas.style.height = "" + video.videoHeight;
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  renderLoop();
+});
